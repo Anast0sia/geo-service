8249f4813b00d7f06449e9192eeb295df537fff0
Anast0sia <160764898+Anast0sia@users.noreply.github.com>
2024-05-22 23:14:17 +0300

    Update MessageSender.java

--- src/test/java/MessageSender.java
+++ src/test/java/MessageSender.java
@@ -1,6 +1,5 @@
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.MockedConstruction;
 import org.mockito.Mockito;
 import ru.netology.entity.Country;
 import ru.netology.entity.Location;
